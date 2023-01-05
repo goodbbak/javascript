9686414e120614d8f41ba5d90c44abb3148ad253
goodbbak <wndks8564@naver.com>
2023-01-05 17:41:19 +0900

    0105학원

--- node/project/app.js
+++ node/project/app.js
@@ -5,9 +5,8 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
 var projectRouter = require("./routes/project");
-var project2Router = require("./routes/project2");
+var loginRouter = require("./routes/login");
 
 var app = express();
 
@@ -22,9 +21,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 app.use("/project", projectRouter);
-app.use("/project2", project2Router);
+app.use("/login", loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
